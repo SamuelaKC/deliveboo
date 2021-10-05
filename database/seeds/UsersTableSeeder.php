5ada18d87873d0289a32c60a4b021f5a3fafcb12
SamuelaKC <carnelli.samuela@gmail.com>
2021-10-05 10:25:54 +0200

    seed 1

---
+++ database/seeds/UsersTableSeeder.php
@@ -0,0 +1,16 @@
+<?php
+
+use Illuminate\Database\Seeder;
+
+class UsersTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $nameRestaurants = ['Il Ristoro', 'La Piadineria', 'K2', 'Girotondo di Pizza', 'Oasi del Gelato', 'Sweet and Fit', 'Il Gatto e La Volpe', 'La Perla', 'Machebuò', 'Fratelli La Bufala', 'Easy Rider Pub', 'Primo Sushi', 'Yama', 'Xiao', 'Girarrosto La Lanterna']
+    }
+}
