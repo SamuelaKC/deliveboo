f1a78684c7112dec3f3e709aeff4fa54f7933166
SamuelaKC <carnelli.samuela@gmail.com>
2021-10-07 11:11:13 +0200

    controller + api

--- routes/api.php
+++ routes/api.php
@@ -17,3 +17,7 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::resource('orders', Api\OrderController::class); 
+Route::resource('plates', Api\PlateController::class); 
+Route::resource('users', Api\UserController::class); 
