8a71cc4e1b418db15f0806533ba0e35023e41dc5
SamuelaKC <carnelli.samuela@gmail.com>
2021-10-07 16:10:31 +0200

    app.js corretta

--- resources/js/app.js
+++ resources/js/app.js
@@ -4,9 +4,9 @@
  * building robust, powerful web applications using Vue and Laravel.
  */
 
-require('./bootstrap');
+require("./bootstrap");
 
-window.Vue = require('vue');
+window.Vue = require("vue");
 
 /**
  * The following block of code may be used to automatically register your
@@ -19,8 +19,7 @@ window.Vue = require('vue');
 // const files = require.context('./', true, /\.vue$/i)
 // files.keys().map(key => Vue.component(key.split('/').pop().split('.')[0], files(key).default))
 
-Vue.component('deliveboo', require('./components/Deliveboo.vue').default);
-Vue.component('form-user', require('./components/FormUser.vue').default);
+Vue.component("deliveboo", require("./components/Deliveboo.vue").default);
 
 /**
  * Next, we will create a fresh Vue application instance and attach it to
@@ -29,5 +28,5 @@ Vue.component('form-user', require('./components/FormUser.vue').default);
  */
 
 const app = new Vue({
-    el: '#app',
+    el: "#app"
 });
