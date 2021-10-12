8a37bbe6632d02cbb76d1fd0ec9ffe569e2de76c
SamuelaKC <carnelli.samuela@gmail.com>
2021-10-12 09:56:00 +0200

    sistemazione main

--- resources/views/plates/index.blade.php
+++ resources/views/plates/index.blade.php
@@ -1,33 +1,48 @@
 @extends('layouts.app') @section('content')
 <div class="container">
     <div class="row">
-        @foreach ($plates as $plate)
-        <div class="col-4 plate">
-            <div class="card-body">
-                <div>Name: {{ $plate->name }}</div>
-                <div>Description:{{ $plate->description }}</div>
-                <div>Price: {{ $plate->price }}</div>
-                <div>Disponibilità: {{ $plate->available }}</div>
-                <div>Picture: {{ $plate->picture }}</div>
-                <a href="{{ route('plates.create') }}">
-                    <button class="btn btn-light">
-                        Aggiungi
-                    </button>
-                </a>
-                <a href="{{ route('plates.edit', $plate) }}">
-                    <button class="btn btn-light">
-                        Modifica
-                    </button>
-                </a>
-                <a href="{{ route('plates.destroy', $plate) }}">
-                    <button class="btn btn-danger">
-                        Cancella
-                    </button>
-                </a>
+        <div class="col">
+            @foreach ($plates as $plate)
+            <div class="row">
+                <div class="col-10">
+                    <h5 class="mt-0 mb-1">Nome: {{ $plate->name }}</h5>
+                    <div>Categoria:</div>
+                    <div>Descrizione: {{ $plate->description }}</div>
+                    <div>Prezzo: {{ $plate->price }}</div>
+                    <div>Disponibilità: {{ $plate->available }}</div>
+                </div>
+                <div class="col-2">
+                    <img
+                        class="img-thumbnail"
+                        src="{{ $plate->picture }}"
+                        alt="{{ $plate->name }}"
+                    />
+                </div>
             </div>
+            <div class="row">
+                <div class="col-2"></div>
+                <div class="col-8 text-center">
+                    <a href="{{ route('plates.create') }}">
+                        <button class="btn btn-light">
+                            Aggiungi
+                        </button>
+                    </a>
+                    <a href="{{ route('plates.edit', $plate) }}">
+                        <button class="btn btn-light">
+                            Modifica
+                        </button>
+                    </a>
+                    <a href="{{ route('plates.destroy', $plate) }}">
+                        <button class="btn btn-danger">
+                            Cancella
+                        </button>
+                    </a>
+                </div>
+                <div class="col-2"></div>
+            </div>
+            <hr />
+            @endforeach
         </div>
-        @endforeach
     </div>
 </div>
-
 @endsection
