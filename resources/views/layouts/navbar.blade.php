184ab4ce67c623e888e38cc9d505c904eed95901
SamuelaKC <carnelli.samuela@gmail.com>
2021-10-13 12:01:49 +0200

    modifica navbar

--- resources/views/layouts/navbar.blade.php
+++ resources/views/layouts/navbar.blade.php
@@ -68,17 +68,11 @@
                             Dashboard
                         </a>
 
-
-                        <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
-
-                            <a class="dropdown-item" href="{{ route('dashboard') }}">
-                                Dashboard
-                            </a>
-
                             <a class="dropdown-item" href="{{ route('logout') }}" onclick="event.preventDefault();
                                 document.getElementById('logout-form').submit();">
                                 {{ __('Logout') }}
                             </a>
+                        <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
 
                         <form
                             id="logout-form"
