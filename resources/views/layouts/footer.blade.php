70708f4c6a6c76e4afe3c2d39fabbb3d3d3fe70b
94tony <dagatatony94@gmail.com>
2021-10-13 09:46:01 +0200

    footer-tony

---
+++ resources/views/layouts/footer.blade.php
@@ -0,0 +1,81 @@
+<!-- Site footer -->
+<footer class="site-footer">
+      <div class="container">
+        <div class="row">
+          <div class="col-sm-6 col-md-3 ">
+            <div class="bkg-categories-footer">
+               <h6>Scopri Deliveroo</h6>
+                <ul class="footer-links"> 
+                    <li><a href="">Investitori</a></li>
+                    <li><a href="">Chi siamo</a></li>
+                    <li><a href="">Ristoranti</a></li>
+                    <li><a href="">Altro</a></li>
+                    <li><a href="">Pressoroom</a></li>
+                    <li><a href="">Il nostro blog</a></li>
+                    <li><a href="">Programmazione</a></li>
+                    <li><a href="">Design</a></li>
+                    <li><a href="">Lavora con noi</a></li>
+                    <li><a href="">Diventa nostro partner</a></li>
+                </ul>
+            </div>
+          
+          </div>
+
+          <div class="col-xs-6 col-md-3 ">
+            <div class="bkg-categories-footer">
+                <h6>Note legali</h6>
+                <ul class="footer-links">
+                    <li><a href="">Termini e condizioni</a></li>
+                    <li><a href="">Informativa sulla privacy</a></li>
+                    <li><a href="">Cookies</a></li>
+                </ul>
+            </div>
+            
+          </div>
+
+          
+          <div class="col-xs-6 col-md-3 mr-auto   ">
+            <div class="bkg-categories-footer">
+                <h6>Aiuto</h6>
+                <ul class="footer-links">
+                    <li><a href="">Contatti</a></li>
+                    <li><a href="">FAQ</a></li>
+                    <li><a href="">Tipi di cucina</a></li>
+                    <li><a href="">Mappa del sito</a></li>
+                </ul>
+            </div>
+          </div>
+
+          <div class="col-xs-6 col-md-3 mr-auto  ">
+            <div class="bkg-categories-footer">
+                <h6>Porta Deliveroo con te</h6>
+                <ul class="footer-links">
+                    <li><a href="">Scarica su App Store</a></li>
+                    <li><a href="">Disponibile su Google Play</a></li>
+                </ul>
+            </div>
+          </div>
+          
+        </div>
+        <hr>
+      </div>
+      <div class="container ">
+        <div class="row d-flex justify-content-between">
+          <div class="col-md-8 col-sm-6 col-xs-12">
+            <ul class="social-icons1">
+              <li><a class="facebook" href="#"><i class="fa fa-facebook"></i></a></li>
+              <li><a class="twitter" href="#"><i class="fa fa-twitter"></i></a></li>
+            </ul>
+          </div>
+          <div class="col-md-4 col-sm-6 col-xs-12">
+            <p class="copyright-text">Copyright &copy; 2021  Deliveboo 
+            
+            </p>
+          </div>
+        </div>
+      </div>
+
+          
+        </div>
+      </div>
+</footer>
