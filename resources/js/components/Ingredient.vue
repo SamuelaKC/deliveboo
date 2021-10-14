a2084181d31fb71922db71d5326189107f7e9405
VinRu5 <vincenzorusso.inbox@gmail.com>
2021-10-14 16:11:40 +0200

    sistemazione ingredient

--- resources/js/components/Ingredient.vue
+++ resources/js/components/Ingredient.vue
@@ -1,40 +1,48 @@
 <template>
-  <div class="background-opacity">
-    <div class="deliveboo-card ingredient-box">
-      <button @click="$emit('closeModal')" class="btn btn-danger btn-sm">
-        <i class="fa fa-times"></i>
-      </button>
-      <div class="img-container">
-        <img :src="{{ plateImg }}" alt="" /> <!--ToDO da sistemare l'src-->
-      </div>
-      <div v-if="ingredients.length > 0">
-        <div
-          v-for="(ingredient, index) in ingredients"
-          :key="ingredient.id"
-          class="form-check"
-        >
-          <input
-            type="checkbox"
-            :id="`ingredient-${ingredient.id}`"
-            v-model="ingredient.value"
-            @click="$emit('flagAddToCart', index)"
-            class="form-check-input"
-          />
-          <label :for="`ingredient-${ingredient.id}`" class="form-check-label">
-            {{ ingredient.name }}
-          </label>
+  <div class="background-opacity row">
+    <div class="deliveboo-card col-4">
+      <div class="ingredient-box">
+        <button @click="$emit('closeModal')" class="btn btn-danger btn-sm button-close">
+          <i class="fa fa-times"></i>
+        </button>
+        <div class="img-container">
+          <img :src="plateImg" alt="foto piatto" />
+          <!--ToDO da sistemare l'src-->
         </div>
+        <div v-if="ingredients.length > 0" class="check-ingredient">
+          <div
+            v-for="(ingredient, index) in ingredients"
+            :key="ingredient.id"
+            class="form-check"
+          >
+            <input
+              type="checkbox"
+              :id="`ingredient-${ingredient.id}`"
+              v-model="ingredient.value"
+              @click="$emit('flagAddToCart', index)"
+              class="form-check-input"
+            />
+            <label
+              :for="`ingredient-${ingredient.id}`"
+              class="form-check-label"
+            >
+              {{ ingredient.name }}
+            </label>
+          </div>
+        </div>
+        <div class="quantity-box">
+          <span @click="$emit('plusQuantity')">
+            <i class="fa fa-plus-circle"></i>
+          </span>
+          <input type="text" v-model="quantityOfPlate" class="input-quantity" />
+          <span @click="$emit('minusQuantity')">
+            <i class="fa fa-minus-circle"></i>
+          </span>
+        </div>
+        <button @click="$emit('sendIn', plateId)" class="btn btn-primary">
+          Aggiungi al Carrello
+        </button>
       </div>
-      <div>
-        <span @click="$emit('plusQuantity')">
-          <i class="fa fa-plus-circle"></i>
-        </span>
-        <input type="text" v-model="quantityOfPlate" class="input-quantity"/>
-        <span @click="$emit('minusQuantity')">
-          <i class="fa fa-minus-circle"></i>
-        </span>
-      </div>
-      <button @click="$emit('sendIn', plateId)" class="btn btn-primary">Aggiungi al Carrello</button>
     </div>
   </div>
 </template>
