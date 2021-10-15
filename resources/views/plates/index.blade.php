82f0ede7afebd5bd2e324e3ab3488e31e4dfabb0
SamuelaKC <carnelli.samuela@gmail.com>
2021-10-15 20:37:57 +0200

    da sistemare nomi duplicati

--- resources/views/plates/index.blade.php
+++ resources/views/plates/index.blade.php
@@ -1,14 +1,25 @@
 @extends('layouts.app') @section('content')
 <div class="container">
     <div class="row">
+        {{-- @dd($plates) --}}
         <div class="col">
-            @foreach ($plates as $plate)
+            @foreach ($plates as $categoryId => $cat)
+            @foreach ($cat as $plate)
+                         @foreach ($categories as $category )
+                        <h2>{{$category->id === $plate->category_id ? $category->name : ''}}</h2>
+                        @endforeach
             <div class="row">
+                <h2></h2>
                 <div class="col-10">
                     <h5 class="mt-0 mb-1">Nome: {{ $plate->name }}</h5>
-                    <div>Categoria:</div>
+                    {{-- <div>
+                        Categoria:
+                         @foreach ($categories as $category )
+                        {{$category->id === $plate->category_id ? $category->name : ''}}
+                        @endforeach
+                    </div> --}}
                     <div>Descrizione: {{ $plate->description }}</div>
-                    <div>Prezzo: {{ $plate->price }}</div>
+                    <div>Prezzo: € {{ number_format ( $plate->price, 2) }}</div>
                     <div>Disponibilità: {{ $plate->available }}</div>
                 </div>
                 <div class="col-2">
@@ -29,9 +40,11 @@
                     </a>
                 </div>
                 <div class="col-12 col-md-3 col-lg-3 text-center">
-                    <form action="{{ route('plates.destroy', $plate) }}" method="POST">
-                        @csrf
-                        @method('DELETE')
+                    <form
+                        action="{{ route('plates.destroy', $plate) }}"
+                        method="POST"
+                    >
+                        @csrf @method('DELETE')
                         <button class="btn btn-danger">
                             Cancella
                         </button>
@@ -40,6 +53,8 @@
                 <div class="col-md-3 col-lg-3"></div>
             </div>
             <hr />
+            
+            @endforeach
             @endforeach
         </div>
     </div>
