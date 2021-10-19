2345340f7e733771c8bb2cd01df6b7dbf4a76996
SamuelaKC <carnelli.samuela@gmail.com>
2021-10-19 20:20:10 +0200

    pagina errore + loghi

--- resources/views/error/index.blade.php
+++ resources/views/error/index.blade.php
@@ -1,8 +1,28 @@
-
-@extends('layouts.app')
-
-@section('content')
-<div class="container">
-Non puoi accedere a questa pagina
+@extends('layouts.app') @section('content')
+<div class="container p-3">
+    <div class="row justify-content-center align-items-center">
+        <div class="col-4">
+            <img
+                src="/image/logo/deliveboo_angry1.svg"
+                alt="Logo Deliveboo Angry"
+                class="svg-color-red"
+            />
+        </div>
+        <div class="col-8"><h1>Qui non puoi passare!</h1></div>
+    </div>
 </div>
+
+<div class="container p-3">
+    <div class="row justify-content-center align-items-center">
+        <div class="col-4">
+            <img
+                src="/image/logo/deliveboo_sorry1.svg"
+                alt="Logo Deliveboo Sorry"
+                class="svg-color-yellow"
+            />
+        </div>
+        <div class="col-8"><h1>Mi spiace, qui non puoi entrare!</h1></div>
+    </div>
+</div>
+
 @endsection
