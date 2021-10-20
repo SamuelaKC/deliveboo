a49cd0f5cf43ee347e67afa2ff04ff2575506d8e
SamuelaKC <carnelli.samuela@gmail.com>
2021-10-20 09:24:02 +0200

    modificato layout ordini

--- resources/views/orders/index.blade.php
+++ resources/views/orders/index.blade.php
@@ -1,54 +1,47 @@
 @extends('layouts.app') @section('content')
 <div class="container">
-    <div class="row">
+    <div class="row justify-content-center">
         <div class="col-12  m-3">
             <h1 class=" font-header">I tuoi ordini</h1>
         </div>
 
-        <div class="col">
+        <div class="col-10">
             @foreach ($orders as $order)
-            <div class="row">
+            <div class="row justify-content-center">
                 <div class="col">
-                    <div class="bold-table m-2">Dettagli cliente</div>
+                    <div class="bold-table m-t-2">Dettagli cliente</div>
                     <div>Nome: {{ $order->name_surname }}</div>
                     <div>Indirizzo: {{ $order->address }}</div>
                     <div>Numero di Telefono: {{ $order->phone_number }}</div>
-                    <div class="bold-table m-2">Dettagli dell'ordine</div>
-                    <div class="row">
-                        <div
-                            class="col-md-4 d-none d-xs-none d-sm-none bold-table"
-                        >
+                    <div class="bold-table m-t-2">
+                        Dettagli dell'ordine
+                    </div>
+                    <div class="m-2">
+                        Aggiunte all'ordine: {{ $order->details }}
+                    </div>
+                    <div class="row justify-content-center">
+                        <div class="col-md-2 d-none d-md-block  bold-table">
+                            Q.TA'
+                        </div>
+                        <div class="col-md-7 d-none d-md-block  bold-table">
                             PIATTO
                         </div>
-                        <div
-                            class="col-md-4 d-none d-xs-none d-sm-none bold-table"
-                        >
-                            AGGIUNTA
-                        </div>
-                        <div
-                            class="col-md-2 d-none d-xs-none d-sm-none bold-table"
-                        >
-                            QUANTITA'
-                        </div>
-                        <div
-                            class="col-md-2 d-none d-xs-none d-sm-none bold-table"
-                        >
+                        <div class="col-md-2 d-none d-md-block  bold-table">
                             PREZZO
                         </div>
                     </div>
-                    <div class="row">
-                        <div class="col-5 col-md-4">
-                            @foreach ($order->plate as $plate )
-                            <div>{{ $plate->name }}</div>
-                            @endforeach
-                        </div>
-                        <div class="col-5 col-md-4">{{ $order->details }}</div>
-                        <div class="col-5 col-md-2">
+                    <div class="row justify-content-center">
+                        <div class="col-1 col-md-2">
                             @foreach ($order->plate as $plate )
                             <div>{{$plate->pivot->quantity}}</div>
                             @endforeach
                         </div>
-                        <div class="col-5 col-md-2">
+                        <div class="col-6 col-md-7 text-nowrap">
+                            @foreach ($order->plate as $plate )
+                            <div>{{ $plate->name }}</div>
+                            @endforeach
+                        </div>
+                        <div class="col-4 col-md-2 border-bottom">
                             @foreach ($order->plate as $plate )
                             <div class="text-right">
                                 € {{ number_format ( $plate->price, 2) }}
@@ -56,9 +49,11 @@
                             @endforeach
                         </div>
                     </div>
-                    <div class="row">
-                        <div class="col-8 col-md-10 bold-table">TOTALE:</div>
-                        <div class="col-2 col-md-2">
+                    <div class="row justify-content-center border-bottom">
+                        <div class="col-8 col-md-9 bold-table">
+                            TOTALE:
+                        </div>
+                        <div class="col-4 col-md-2">
                             <div class="bold-table text-right">
                                 € {{ number_format($order->total_price, 2) }}
                             </div>
@@ -66,7 +61,6 @@
                     </div>
                 </div>
             </div>
-            <hr />
             @endforeach
         </div>
     </div>
